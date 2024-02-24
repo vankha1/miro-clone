01b1f7bed80f0241d2a8c89f692f0df613d44bf8
vankha1 <vovankha2003@gmail.com>
2024-02-24 23:20:57 +0700

    feat: add convex for backend and clerk for auth

---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!; // Exclamation Mark used for not undefined
+
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = ({
+  children,
+}: ConvexClientProviderProps) => {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
