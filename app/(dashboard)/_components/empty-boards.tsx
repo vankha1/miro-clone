78fe2bb1128b5edbb877a12d7bc6957300a2d6e5
vankha1 <vovankha2003@gmail.com>
2024-08-30 17:54:33 +0700

    chore: customize use api mutation hook

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -2,18 +2,19 @@
 
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 import { useOrganization } from "@clerk/nextjs";
 import { useMutation } from "convex/react";
 import Image from "next/image";
 
 export const EmptyBoards = () => {
   const { organization } = useOrganization();
-  const create = useMutation(api.board.create);
+  const { mutate, pending } = useApiMutation(api.board.create);
 
   const handleCreateBoard = () => {
     if (!organization) return;
 
-    create({
+    mutate({
       title: "New board",
       orgId: organization.id,
     });
@@ -27,7 +28,7 @@ export const EmptyBoards = () => {
       </p>
 
       <div className="mt-6">
-        <Button onClick={handleCreateBoard} size="lg">
+        <Button disabled={pending} onClick={handleCreateBoard} size="lg">
           Create board
         </Button>
       </div>
