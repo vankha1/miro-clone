34f496016515b2b4dff0676ce127fd24844bcaf2
vankha1 <vovankha2003@gmail.com>
2024-08-31 15:54:45 +0700

    feat: define user favorites schema and favorite mutation

--- convex/board.ts
+++ convex/board.ts
@@ -1,89 +1,168 @@
 import { v } from "convex/values";
 import { mutation } from "./_generated/server";
-import { title } from "process";
 
 const images = [
-  "/placeholders/1.svg",
-  "/placeholders/2.svg",
-  "/placeholders/3.svg",
-  "/placeholders/4.svg",
-  "/placeholders/5.svg",
-  "/placeholders/6.svg",
-  "/placeholders/7.svg",
-  "/placeholders/8.svg",
-  "/placeholders/9.svg",
-  "/placeholders/10.svg",
+    "/placeholders/1.svg",
+    "/placeholders/2.svg",
+    "/placeholders/3.svg",
+    "/placeholders/4.svg",
+    "/placeholders/5.svg",
+    "/placeholders/6.svg",
+    "/placeholders/7.svg",
+    "/placeholders/8.svg",
+    "/placeholders/9.svg",
+    "/placeholders/10.svg",
 ];
 
 export const create = mutation({
-  args: {
-    title: v.string(),
-    orgId: v.string(),
-  },
-  handler: async (ctx, args) => {
-    const identity = await ctx.auth.getUserIdentity();
+    args: {
+        title: v.string(),
+        orgId: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
 
-    if (!identity) {
-      throw new Error("Unauthorized");
-    }
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
 
-    const randomImage = images[Math.floor(Math.random() * images.length)];
+        const randomImage = images[Math.floor(Math.random() * images.length)];
 
-    const boards = await ctx.db.insert("boards", {
-      title: args.title,
-      orgId: args.orgId,
-      authorId: identity.subject,
-      authorName: identity.name!,
-      imageUrl: randomImage,
-    });
-    return boards;
-  },
+        const boards = await ctx.db.insert("boards", {
+            title: args.title,
+            orgId: args.orgId,
+            authorId: identity.subject,
+            authorName: identity.name!,
+            imageUrl: randomImage,
+        });
+        return boards;
+    },
 });
 
 export const remove = mutation({
-  args: {
-    id: v.id("boards"),
-  },
-  handler: async (ctx, args) => {
-    const identity = await ctx.auth.getUserIdentity();
+    args: {
+        id: v.id("boards"),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
 
-    if (!identity) {
-      throw new Error("Unauthorized");
-    }
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
 
-    // TODO: Later check to delete favorite relation as well
+        // TODO: Later check to delete favorite relation as well
 
-    await ctx.db.delete(args.id);
-  },
+        await ctx.db.delete(args.id);
+    },
 });
 
 export const update = mutation({
-  args: {
-    id: v.id("boards"),
-    title: v.string(),
-  },
-  handler: async (ctx, args) => {
-    const identity = await ctx.auth.getUserIdentity();
+    args: {
+        id: v.id("boards"),
+        title: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
 
-    if (!identity) {
-      throw new Error("Unauthorized");
-    }
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
 
-    const title = args.title.trim();
-    if (!title) {
-      throw new Error("Title is required");
-    }
+        const title = args.title.trim();
+        if (!title) {
+            throw new Error("Title is required");
+        }
 
-    if (title.length > 60) {
-      throw new Error("Title is too long");
-    }
+        if (title.length > 60) {
+            throw new Error("Title is too long");
+        }
 
-    // TODO: Later check to delete favorite relation as well
+        // TODO: Later check to delete favorite relation as well
 
-    const board = await ctx.db.patch(args.id, {
-      title: args.title,
-    });
+        const board = await ctx.db.patch(args.id, {
+            title: args.title,
+        });
 
-    return board;
-  },
+        return board;
+    },
+});
+
+export const favorite = mutation({
+    args: {
+        id: v.id("boards"),
+        orgId: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const board = await ctx.db.get(args.id);
+
+        if (!board) {
+            throw new Error("Board not found");
+        }
+
+        const existingFavorite = await ctx.db
+            .query("userFavorites")
+            .withIndex("by_user_board_org", (q) =>
+                // The order of eq must match with the order in schema
+                q
+                    .eq("userId", identity.subject)
+                    .eq("boardId", board._id)
+                    .eq("orgId", args.orgId)
+            )
+            .unique();
+
+        if (existingFavorite) {
+            throw new Error("Board alread favorited");
+        }
+
+        ctx.db.insert("userFavorites", {
+            orgId: args.orgId,
+            userId: identity.subject,
+            boardId: board._id,
+        });
+
+        return board;
+    },
+});
+
+export const unfavorite = mutation({
+    args: {
+        id: v.id("boards"),
+        orgId: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const board = await ctx.db.get(args.id);
+
+        if (!board) {
+            throw new Error("Board not found");
+        }
+
+        const existingFavorite = await ctx.db
+            .query("userFavorites")
+            .withIndex("by_user_board", (q) =>
+                q
+                    .eq("userId", identity.subject)
+                    .eq("boardId", board._id)
+            )
+            .unique();
+
+        if (!existingFavorite) {
+            throw new Error("Favorited board not found");
+        }
+
+        await ctx.db.delete(existingFavorite._id);
+
+        return board;
+    },
 });
