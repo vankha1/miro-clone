ab40323f7025f270250d0c74feb2ed60cd4d6611
vankha1 <vovankha2003@gmail.com>
2024-09-02 11:16:07 +0700

    feat (IMPORTANT): move to front and send to back a layer

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -6,7 +6,7 @@ import { ColorPicker } from "./color-picker";
 import { useDeleteLayers } from "@/hooks/use-delete-layers";
 import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
-import { Trash2 } from "lucide-react";
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 
 interface ISelectionTools {
     camera: Camera;
@@ -17,14 +17,60 @@ export const SelectionTools = memo(
     ({ camera, setLastUsedColor }: ISelectionTools) => {
         const selection = useSelf((me) => me.presence.selection);
 
-        const setFill = useMutation(({ storage }, fill: Color) => {
-            const liveLayers = storage.get("layers");
-            setLastUsedColor(fill);
+        const moveToFront = useMutation(
+            ({ storage }) => {
+                const liveLayerIds = storage.get("layerIds");
+                const indices: number[] = [];
 
-            selection?.forEach((id) => {
-                liveLayers.get(id)?.set("fill", fill);
-            });
-        }, [selection, setLastUsedColor]);
+                const arr = liveLayerIds.toArray();
+
+                for (let i = 0; i < arr.length; i++) {
+                    if (selection?.includes(arr[i])) {
+                        indices.push(i);
+                    }
+                }
+
+                for (let i = indices.length - 1; i >= 0; i--) {
+                    liveLayerIds.move(
+                        indices[i],
+                        arr.length - 1 - (indices.length - 1 - i)
+                    );
+                }
+            },
+            [selection]
+        );
+
+        const moveToBack = useMutation(
+            ({ storage }) => {
+                const liveLayerIds = storage.get("layerIds");
+                const indices: number[] = [];
+
+                const arr = liveLayerIds.toArray();
+
+                for (let i = 0; i < arr.length; i++) {
+                    if (selection?.includes(arr[i])) {
+                        indices.push(i);
+                    }
+                }
+
+                for (let i = 0; i < indices.length; i++) {
+                    liveLayerIds.move(indices[i], i);
+                }
+            },
+            [selection]
+        );
+
+        const setFill = useMutation(
+            ({ storage }, fill: Color) => {
+                const liveLayers = storage.get("layers");
+                setLastUsedColor(fill);
+
+                selection?.forEach((id) => {
+                    liveLayers.get(id)?.set("fill", fill);
+                });
+            },
+            [selection, setLastUsedColor]
+        );
 
         const deleteLayers = useDeleteLayers();
 
@@ -49,9 +95,28 @@ export const SelectionTools = memo(
             >
                 <ColorPicker onChange={setFill} />
 
-                <div
-                    className="flex items-center pl-2 ml-2 border-l border-neutral-200"
-                >
+                <div className="flex flex-col gap-y-0.5">
+                    <Hint label="Bring to font">
+                        <Button
+                            variant="board"
+                            size="icon"
+                            onClick={moveToFront}
+                        >
+                            <BringToFront />
+                        </Button>
+                    </Hint>
+                    <Hint label="Send to back">
+                        <Button
+                            variant="board"
+                            size="icon"
+                            onClick={moveToBack}
+                        >
+                            <SendToBack />
+                        </Button>
+                    </Hint>
+                </div>
+
+                <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
                     <Hint label="Delete">
                         <Button
                             variant="board"
